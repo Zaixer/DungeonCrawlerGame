6265fe26d84427c417ef2ade4522bdec09f331d9
Zaixer <jacobsen_dennis@hotmail.com>
2015-08-22 23:45:28 +0200

    Started working on buttons and refactored

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,14 +3,13 @@
 public class GameController : MonoBehaviour {
     public GameObject background;
     public GameObject unit;
-
-    private const float SCREEN_WIDTH = 8f;
-    private const float SCREEN_HEIGHT = 6f;
-    private const float BACKGROUND_Y_POSITION = SCREEN_HEIGHT / 4f;
-    private const float UNIT_Y_POSITION = 0.5f;
+    public GameObject buttonWalkLeft;
+    public GameObject buttonWalkRight;
 
     void Start () {
-        Instantiate(background, new Vector3(0, BACKGROUND_Y_POSITION), Quaternion.identity);
-        Instantiate(unit, new Vector3(-3f, UNIT_Y_POSITION), Quaternion.identity);
+        Instantiate(background, ScreenPositionHelper.GetFromPixels(0, 150), Quaternion.identity);
+        Instantiate(unit, ScreenPositionHelper.GetFromPixels(-300, 50), Quaternion.identity);
+        Instantiate(buttonWalkLeft);
+        Instantiate(buttonWalkRight);
     }
 }
---
+++ Assets/Scripts/ScreenPositionHelper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class ScreenPositionHelper {
+    private const float UNITS_PER_PIXEL = 100f;
+
+    public static Vector3 GetFromPixels(int pixelsX, int pixelsY)
+    {
+        return new Vector3(pixelsX / UNITS_PER_PIXEL, pixelsY / UNITS_PER_PIXEL);
+    }
+}
