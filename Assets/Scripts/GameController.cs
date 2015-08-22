09b7fd36ad878689d89ae4570b531d7bd8db12b2
Zaixer <jacobsen_dennis@hotmail.com>
2015-08-23 00:41:05 +0200

    Fixed non working scene

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,13 +3,9 @@
 public class GameController : MonoBehaviour {
     public GameObject background;
     public GameObject unit;
-    public GameObject buttonWalkLeft;
-    public GameObject buttonWalkRight;
 
     void Start () {
         Instantiate(background, ScreenPositionHelper.GetFromPixels(0, 150), Quaternion.identity);
         Instantiate(unit, ScreenPositionHelper.GetFromPixels(-300, 50), Quaternion.identity);
-        Instantiate(buttonWalkLeft);
-        Instantiate(buttonWalkRight);
     }
 }
