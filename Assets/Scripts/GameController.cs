d8f9731769948937db2dadd1aedddbfcbc277a78
Zaixer <jacobsen_dennis@hotmail.com>
2015-08-22 21:27:23 +0200

    Setup camera with background and unit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject background;
+    public GameObject unit;
+
+    private const float SCREEN_WIDTH = 8f;
+    private const float SCREEN_HEIGHT = 6f;
+    private const float BACKGROUND_Y_POSITION = SCREEN_HEIGHT / 4f;
+    private const float UNIT_Y_POSITION = 0.5f;
+
+    void Start () {
+        Instantiate(background, new Vector3(0, BACKGROUND_Y_POSITION), Quaternion.identity);
+        Instantiate(unit, new Vector3(-3f, UNIT_Y_POSITION), Quaternion.identity);
+    }
+}
