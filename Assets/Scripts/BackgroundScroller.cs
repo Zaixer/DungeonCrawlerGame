dd76feb9db90a3192f814398482ed3db39944db6
Zaixer <jacobsen_dennis@hotmail.com>
2015-08-23 14:40:44 +0200

    Move background right and left with buttons

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -1,10 +1,51 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class BackgroundScroller : MonoBehaviour {
-    public float speed = 0.1f;
+    public float speed = 0.01f;
 
-	void Update () {
-        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(Time.time * speed, 0f);
+    private MoveStatus moveStatus = MoveStatus.None;
+
+    public void MoveLeft()
+    {
+        moveStatus = MoveStatus.Left;
+    }
+
+    public void MoveRight()
+    {
+        moveStatus = MoveStatus.Right;
+
+    }
+
+    public void StopMoving()
+    {
+        moveStatus = MoveStatus.None;
+    }
+
+    void Update()
+    {
+        switch (moveStatus)
+        {
+            case MoveStatus.Left:
+                MoveBackground(-speed);
+                break;
+            case MoveStatus.Right:
+                MoveBackground(speed);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void MoveBackground(float xDistance)
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.material.mainTextureOffset = new Vector2(renderer.material.mainTextureOffset.x + xDistance, 0f);
+    }
+
+    private enum MoveStatus
+    {
+        None,
+        Left,
+        Right
     }
 }
