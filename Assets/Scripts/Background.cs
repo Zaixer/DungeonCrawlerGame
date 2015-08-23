47b1ef7baa30656df8f6540f51c18dd99d7134e9
Zaixer <jacobsen_dennis@hotmail.com>
2015-08-23 16:40:33 +0200

    New graphics and resolution and parallax background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float moveDistance = 0.01f;
+
+    private Renderer _renderer;
+
+    public void MoveBackground()
+    {
+        _renderer.material.mainTextureOffset = new Vector2(_renderer.material.mainTextureOffset.x + moveDistance, 0f);
+    }
+
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {    
+    private MoveStatus _moveStatus = MoveStatus.None;
+    private ICollection<Background> _backgrounds = new List<Background>();
+
+    public void MoveLeft()
+    {
+        _moveStatus = MoveStatus.Left;
+    }
+
+    public void MoveRight()
+    {
+        _moveStatus = MoveStatus.Right;
+
+    }
+
+    public void StopMoving()
+    {
+        _moveStatus = MoveStatus.None;
+    }
+
+    void Start()
+    {
+        var gameObjectsWithBackground = GameObject.FindGameObjectsWithTag("Background");
+        foreach (var gameObjectWithBackground in gameObjectsWithBackground)
+        {
+            _backgrounds.Add(gameObjectWithBackground.GetComponent<Background>());
+        }
+    }
+
+    void Update()
+    {
+        switch (_moveStatus)
+        {
+            case MoveStatus.Left:
+                foreach (var background in _backgrounds)
+                {
+                    background.MoveBackground();
+                }
+                break;
+            case MoveStatus.Right:
+                foreach (var background in _backgrounds)
+                {
+                    background.MoveBackground();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private enum MoveStatus
+    {
+        None,
+        Left,
+        Right
+    }
+}
